43fea4f825a6e6e005c3e7b2004844d38911a016
yearing1017 <33566006+yearing1017@users.noreply.github.com>
2019-12-09 11:47:08 +0800

    Create Pytorch_CNN.py

---
+++ Pytorch_CNN.py
@@ -0,0 +1,57 @@
+import torch 
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+
+# gpu
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# 超参数
+num_epochs = 5
+num_classes = 10
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                           train=True, 
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                          train=False, 
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size, 
+                                          shuffle=False)
+
+# 网络搭建
+class ConvNet(nn.Moudle):
+    def __init__(self):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
